e531048403fb5233ee827e10337818376d68b095
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-07-10 09:55:57 -0400

    Added a server to index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const http = require("http");
+
+//base url is "/". If a request is made, server will feed that.
+
+const server = http.createServer((req, res) => {
+  //to go to the website we are just LOADING the html files
+  console.log(req.url);
+});
+
+const PORT = process.env.port || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
