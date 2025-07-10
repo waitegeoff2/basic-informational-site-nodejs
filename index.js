aa78556dc43c147876e07282117998617da71743
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-07-10 10:57:57 -0400

    Added details to server to redirect to pages Made a dynamic filepath to take to proper URL

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const http = require("http");
-
-//base url is "/". If a request is made, server will feed that.
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
-  //to go to the website we are just LOADING the html files
-  console.log(req.url);
+
+  //dynamic file path. Equals the directory name, then pages, and then the request url (if not /)
+    let filePath = path.join(__dirname, 
+        'pages', 
+        req.url === '/' ? 'index.html' : req.url
+    );
+
+    //gets the extension, and then creates a contentType to serve with writeHead
+    let extname = path.extname(filePath)
+
+    let contentType = 'text/html'
+
+    switch (extname) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/jpg";
+            break;
+    }
+
+    //readfile takes a path to the content, then says what to do with an error, 
+    // and then what to do with that content (display it in this case)
+
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, 'pages', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'contentType': 'text/html' })
+                    res.end(content, 'utf8')
+                })
+            } else {
+                res.writeHead(500)
+                res.end(`Server error: ${error.code}`)
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType })
+            res.end(content, 'utf8')
+        }
+    })
 });
 
 const PORT = process.env.port || 3000;
